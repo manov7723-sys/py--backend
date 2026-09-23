f19f935da96580d3a5faeaa95ff770502fc1ae2f
manov7723-sys <manov7723@gmail.com>
2026-09-23 17:54:42 +0530

    Add dev-one-dev-db infrastructure (Terraform)

---
+++ infra/dev-one-dev-db/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.4"
+  required_providers {
+    aws    = { source = "hashicorp/aws",    version = "~> 5.60" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
